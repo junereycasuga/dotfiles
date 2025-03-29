7ea7771fca702fedd17383d92b4887b9aa9bedcd
Junerey <junerey.casuga@gmail.com>
2025-03-30 00:38:09 +0700

    migrate to lazy from packer

--- nvim/.config/nvim/lua/junerey/init.lua
+++ nvim/.config/nvim/lua/junerey/init.lua
@@ -1,15 +1,9 @@
+-- Load core configurations
 require("junerey.remap")
 require("junerey.set")
 
--- default colorscheme
--- vim.cmd([[
--- 	let g:jellybeans_overrides = {
--- \    'background': { 'ctermbg': 'none', '256ctermbg': 'none' },
--- \}
--- if has('termguicolors') && &termguicolors
---     let g:jellybeans_overrides['background']['guibg'] = 'none'
--- endif
--- ]])
--- vim.cmd([[ colorscheme jellybeans]])
+-- Initialize lazy.nvim
+require("junerey.lazy-setup")
 
+-- Set colorscheme
 vim.cmd([[ colorscheme catppuccin ]])
