44b5a715028971b8945b8e9c173209d3970c5f8c
Junerey <junerey@salary-hero.com>
2025-04-28 13:32:37 +0700

    add kulala.nvim

--- nvim/.config/nvim/lua/junerey/lazy-plugins.lua
+++ nvim/.config/nvim/lua/junerey/lazy-plugins.lua
@@ -1,147 +1,161 @@
 return {
-    -- Git integration
-    { "tpope/vim-fugitive" },
-    { "lewis6991/gitsigns.nvim" },
+	-- Git integration
+	{ "tpope/vim-fugitive" },
+	{ "lewis6991/gitsigns.nvim" },
 
-    -- Editor enhancements
-    { "tpope/vim-sleuth" },
-    { "nvim-lua/plenary.nvim" },
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    },
+	-- Editor enhancements
+	{ "tpope/vim-sleuth" },
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-    -- Telescope fuzzy finder
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.4",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local telescope = require("telescope")
-            local telescopeConfig = require("telescope.config")
-            local builtin = require("telescope.builtin")
-            local actions = require("telescope.actions")
+	-- Telescope fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			local telescopeConfig = require("telescope.config")
+			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
 
-            local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+			local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
-            table.insert(vimgrep_arguments, "--hidden")
-            table.insert(vimgrep_arguments, "--glob")
-            table.insert(vimgrep_arguments, "!**/.git/*")
+			table.insert(vimgrep_arguments, "--hidden")
+			table.insert(vimgrep_arguments, "--glob")
+			table.insert(vimgrep_arguments, "!**/.git/*")
 
-            telescope.setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-j>"] = {
-                                actions.move_selection_next,
-                                type = "action",
-                                opts = { nowait = true, silent = true },
-                            },
-                            ["<C-k>"] = {
-                                actions.move_selection_previous,
-                                type = "action",
-                                opts = { nowait = true, silent = true },
-                            },
-                        },
-                    },
-                    vimgrep_arguments = vimgrep_arguments
-                },
-                pikcers = {
-                    find_files = {
-                        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
-                    }
-                }
-            })
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = {
+								actions.move_selection_next,
+								type = "action",
+								opts = { nowait = true, silent = true },
+							},
+							["<C-k>"] = {
+								actions.move_selection_previous,
+								type = "action",
+								opts = { nowait = true, silent = true },
+							},
+						},
+					},
+					vimgrep_arguments = vimgrep_arguments,
+				},
+				pikcers = {
+					find_files = {
+						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+					},
+				},
+			})
 
-            local keymap = vim.keymap
-            keymap.set("n", "<leader>ff", builtin.find_files, {})
-            keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            keymap.set("n", "<C-p>", builtin.git_files, {})
-            keymap.set("n", "<leader>vh", builtin.help_tags, {})
-        end
-    },
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>ff", builtin.find_files, {})
+			keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			keymap.set("n", "<C-p>", builtin.git_files, {})
+			keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		end,
+	},
 
-    -- Treesitter for better syntax highlighting
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPre", "BufNewFile" },
-        build = ":TSUpdate",
-    },
+	-- Treesitter for better syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+	},
 
-    -- File explorer
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-        },
-    },
+	-- File explorer
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	},
 
-    -- Tmux integration
-    { "alexghergh/nvim-tmux-navigation" },
+	-- Tmux integration
+	{ "alexghergh/nvim-tmux-navigation" },
 
-    -- Color schemes
-    { "rafi/awesome-vim-colorschemes" },
-    { "catppuccin/nvim",                name = "catppuccin" },
+	-- Color schemes
+	{ "rafi/awesome-vim-colorschemes" },
+	{ "catppuccin/nvim", name = "catppuccin" },
 
-    -- LSP configuration
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            {
-                "williamboman/mason.nvim",
-                build = ":MasonUpdate",
-            },
-            { "williamboman/mason-lspconfig.nvim" },
+	-- LSP configuration
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{
+				"williamboman/mason.nvim",
+				build = ":MasonUpdate",
+			},
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "L3MON4D3/LuaSnip" },
-        },
-    },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	},
 
-    -- Formatting
-    {
-        "nvimtools/none-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
+	-- Formatting
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-    -- Code manipulation
-    { "tpope/vim-commentary" },
-    { "tpope/vim-surround" },
+	-- Code manipulation
+	{ "tpope/vim-commentary" },
+	{ "tpope/vim-surround" },
 
-    -- Status line
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-    -- Auto pairs
-    { "jiangmiao/auto-pairs" },
+	-- Auto pairs
+	{ "jiangmiao/auto-pairs" },
 
-    -- Indentation
-    { "lukas-reineke/indent-blankline.nvim" },
+	-- Indentation
+	{ "lukas-reineke/indent-blankline.nvim" },
 
-    -- Diagnostics
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+	-- Diagnostics
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-    -- Alpha (greeter)
-    {
-        "goolord/alpha-nvim",
-        -- dependencies = { 'echasnovski/mini.icons' },
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-            require'alpha'.setup(require'alpha.themes.dashboard'.config)
-        end,
-    }
+	-- Alpha (greeter)
+	{
+		"goolord/alpha-nvim",
+		-- dependencies = { 'echasnovski/mini.icons' },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.dashboard").config)
+		end,
+	},
+
+	{
+		"mistweaverco/kulala.nvim",
+		keys = {
+			{ "<leader>Rs", desc = "Send request" },
+			{ "<leader>Ra", desc = "Send all requests" },
+			{ "<leader>Rb", desc = "Open scratchpad" },
+		},
+		ft = { "http", "rest" },
+		opts = {
+			-- your configuration comes here
+			global_keymaps = false,
+		},
+	},
 }
