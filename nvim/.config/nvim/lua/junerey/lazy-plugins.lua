10fdaabffec9227491ebd98850ccd25f6841abc5
Junerey <junerey.casuga@gmail.com>
2025-07-16 08:12:55 +0700

    nvim: updated oil config

--- nvim/.config/nvim/lua/junerey/lazy-plugins.lua
+++ nvim/.config/nvim/lua/junerey/lazy-plugins.lua
@@ -94,6 +94,15 @@ return {
 		config = function()
 			require("oil").setup({
 				default_file_explorer = true,
+				delete_to_trash = true,
+				skip_confirm_for_simple_edits = true,
+				view_options = {
+					show_hidden = true,
+					natural_order = true,
+				},
+				win_options = {
+					wrap = true
+				}
 			})
 		end,
 	},
