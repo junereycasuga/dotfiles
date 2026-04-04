b527ed30cebf4b5cb9365b14611641f79f769f12
Junerey <junerey.casuga@gmail.com>
2026-04-04 09:30:21 +0700

    nvim: update lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -134,7 +134,7 @@ return {
 					formatting.stylua,
 				},
 				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
+					if client:supports_method("textDocument/formatting") then
 						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 						vim.api.nvim_create_autocmd("BufWritePre", {
 							group = augroup,
